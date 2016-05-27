ea4c2c30c95a41d26dfc99f0de15b282e6b20d43
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-27 16:45:36 -0400

    make this an ES6 class

--- server/data/User.js
+++ server/data/User.js
@@ -1,142 +1,145 @@
 "use strict";
 
-function User(info) {
-  this.devices = [];
-  this.handlers = [];
-  this.state = {
-    x: 0,
-    y: 0,
-    z: 0,
-    dx: 0,
-    dy: 0,
-    dz: 0,
-    heading: 0,
-    dHeading: 0,
-    isRunning: false,
-    userName: info.userName,
-    app: null
-  };
 const log = require("../core").log;
 
-  this.salt = info.salt;
-  this.hash = info.hash;
-  this.email = info.email;
-}
+class User{
+  constructor(info) {
+    this.devices = [];
+    this.handlers = [];
+    this.state = {
+      x: 0,
+      y: 0,
+      z: 0,
+      dx: 0,
+      dy: 0,
+      dz: 0,
+      heading: 0,
+      dHeading: 0,
+      isRunning: false,
+      userName: info.userName,
+      app: null
+    };
 
-User.prototype.addDevice = function (socket, users) {
-  //
-  // find a slot in which to put the socket
-  //
-  var index = 0;
-  while (index < this.devices.length && this.devices[index]) {
-    ++index;
+    this.salt = info.salt;
+    this.hash = info.hash;
+    this.email = info.email;
   }
 
-  log("Device added for $1", this.state.userName);
-  this.devices[index] = socket;
-  
-  //
-  // bind the events
-  //
-  var handlers = {
-    onUserState: function (state) {
-      this.state.x = state.x;
-      this.state.y = state.y;
-      this.state.z = state.z;
-      this.state.heading = state.heading;
-      this.state.isRunning = state.isRunning;
-      this.state.app = state.app;
-      this.broadcast(users, index, "userState", this.state);
-    }.bind(this),
-    onChat: User.prototype.chat.bind(this, users),
-    onDisconnect: User.prototype.disconnect.bind(this, users, index)
-  };
-  socket.on("userState", handlers.onUserState);
-  socket.on("chat", handlers.onChat);
-  socket.on("logout", handlers.onDisconnect);
-  socket.on("disconnect", handlers.onDisconnect);
-  this.handlers[index] = handlers;
-  
-  //
-  // notify the new client of all of the users currently logged in
-  //
-  var userList = [];
-  users.forEach(function (key, user) {
-    if (user.isConnected()) {
-      userList.push(user.state);
+  addDevice(socket, users) {
+    //
+    // find a slot in which to put the socket
+    //
+    var index = 0;
+    while (index < this.devices.length && this.devices[index]) {
+      ++index;
     }
-  });
-  socket.emit("userList", userList);
 
-  if (index === 0) {
+    log("Device added for $1", this.state.userName);
+    this.devices[index] = socket;
+    
     //
-    // notify all of the users of a new user
+    // bind the events
     //
-    this.broadcast(users, index, "userJoin", this.state);
-  }
-  else {
+    var handlers = {
+      onUserState: function (state) {
+        this.state.x = state.x;
+        this.state.y = state.y;
+        this.state.z = state.z;
+        this.state.heading = state.heading;
+        this.state.isRunning = state.isRunning;
+        this.state.app = state.app;
+        this.broadcast(users, index, "userState", this.state);
+      }.bind(this),
+      onChat: User.prototype.chat.bind(this, users),
+      onDisconnect: User.prototype.disconnect.bind(this, users, index)
+    };
+    socket.on("userState", handlers.onUserState);
+    socket.on("chat", handlers.onChat);
+    socket.on("logout", handlers.onDisconnect);
+    socket.on("disconnect", handlers.onDisconnect);
+    this.handlers[index] = handlers;
+    
     //
-    // notify all of the user's devices that a new device was connected,
-    // then update them on the user's current state.
+    // notify the new client of all of the users currently logged in
     //
-    this.emit(index, "deviceAdded");
-    socket.emit("userState", this.state);
-  }
-};
+    var userList = [];
+    users.forEach(function (key, user) {
+      if (user.isConnected()) {
+        userList.push(user.state);
+      }
+    });
+    socket.emit("userList", userList);
 
-User.prototype.broadcast = function (users, skipIndex) {
-  var args = Array.prototype.slice.call(arguments, 2);
-  users.forEach(function (key, toUser) {
-    if (toUser.state.app === this.state.app) {
-      toUser.emit.bind(toUser, (toUser.state.userName === this.state.userName) ? skipIndex : -1)
+    if (index === 0) {
+      //
+      // notify all of the users of a new user
+      //
+      this.broadcast(users, index, "userJoin", this.state);
+    }
+    else {
+      //
+      // notify all of the user's devices that a new device was connected,
+      // then update them on the user's current state.
+      //
+      this.emit(index, "deviceAdded");
+      socket.emit("userState", this.state);
+    }
+  }
+
+  broadcast(users, skipIndex) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    users.forEach(function (key, toUser) {
+      if (toUser.state.app === this.state.app) {
+        toUser.emit.bind(toUser, (toUser.state.userName === this.state.userName) ? skipIndex : -1)
         .apply(toUser, args);
-    }
-  }.bind(this));
-};
-
-User.prototype.emit = function (skipIndex) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  for (var i = 0; i < this.devices.length; ++i) {
-    if (i !== skipIndex && this.devices[i]) {
-      this.devices[i].emit.apply(this.devices[i], args);
-    }
+      }
+    }.bind(this));
   }
-};
 
-User.prototype.isConnected = function () {
-  var devicesLeft = 0;
-  for (var i = 0; i < this.devices.length; ++i) {
-    if (this.devices[i]) {
-      ++devicesLeft;
+  emit(skipIndex) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < this.devices.length; ++i) {
+      if (i !== skipIndex && this.devices[i]) {
+        this.devices[i].emit.apply(this.devices[i], args);
+      }
     }
   }
-  return devicesLeft > 0;
-};
 
-User.prototype.chat = function (users, text) {
-  log("[$1]: $2", this.state.userName, text);
-  this.broadcast(users, -1, "chat", {
-    userName: this.state.userName,
-    text: text
-  });
-};
+  isConnected() {
+    var devicesLeft = 0;
+    for (var i = 0; i < this.devices.length; ++i) {
+      if (this.devices[i]) {
+        ++devicesLeft;
+      }
+    }
+    return devicesLeft > 0;
+  }
 
-User.prototype.disconnect = function (users, index) {
-  this.devices[index].removeListener("userState", this.handlers[index].onUserState);
-  this.devices[index].removeListener("chat", this.handlers[index].onChat);
-  this.devices[index].removeListener("logout", this.handlers[index].onDisconnect);
-  this.devices[index].removeListener("disconnect", this.handlers[index].onDisconnect);
-  this.devices[index] = null;
-  this.handlers[index] = null;
-  if (this.isConnected()) {
-    log("Device #$1 lost for $2.", index, this.state.userName);
-    this.emit(index, "deviceLost");
+  chat(users, text) {
+    log("[$1]: $2", this.state.userName, text);
+    this.broadcast(users, -1, "chat", {
+      userName: this.state.userName,
+      text: text
+    });
   }
-  else {
-    log("disconnect = $1.", this.state.userName);
-    this.broadcast(users, -1, "userLeft", this.state.userName);
-    this.devices.splice(0);
+
+  disconnect(users, index) {
+    this.devices[index].removeListener("userState", this.handlers[index].onUserState);
+    this.devices[index].removeListener("chat", this.handlers[index].onChat);
+    this.devices[index].removeListener("logout", this.handlers[index].onDisconnect);
+    this.devices[index].removeListener("disconnect", this.handlers[index].onDisconnect);
+    this.devices[index] = null;
+    this.handlers[index] = null;
+    if (this.isConnected()) {
+      log("Device #$1 lost for $2.", index, this.state.userName);
+      this.emit(index, "deviceLost");
+    }
+    else {
+      log("disconnect = $1.", this.state.userName);
+      this.broadcast(users, -1, "userLeft", this.state.userName);
+      this.devices.splice(0);
+    }
   }
-};
+}
 
 module.exports = User;
