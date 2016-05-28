2d206e706e5f00fe21f68843012f2369c60146e5
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-28 14:58:49 -0400

    new task for just generating the templates, not including copying the Primrose source

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 ﻿var gulp = require("gulp"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  data = require("gulp-data"),
-  exec = require("child_process").exec,
-  fs = require("fs"),
-  primroseInfo = require("../Primrose/package.json"),
-  pug = require("gulp-pug"),
-  recurseDirectory = require("./server/recurseDirectory"),
-  rename = require("gulp-rename"),
-  uglify = require("gulp-uglify"),
-  zip = require("gulp-zip"),
-  pathX = /.*\/(.*).js/;
+concat = require("gulp-concat"),
+cssmin = require("gulp-cssmin"),
+data = require("gulp-data"),
+exec = require("child_process").exec,
+fs = require("fs"),
+primroseInfo = require("../Primrose/package.json"),
+pug = require("gulp-pug"),
+recurseDirectory = require("./server/recurseDirectory"),
+rename = require("gulp-rename"),
+uglify = require("gulp-uglify"),
+zip = require("gulp-zip"),
+pathX = /.*\/(.*).js/;
 
 function X(name, cmd, deps){
   gulp.task(name, deps || [], function(cb){
@@ -32,28 +32,28 @@ gulp.task("copy:primrose", ["build:primrose"], function () {
     }
     return f;
   }).concat(["!../Primrose/src/**/*", "!../Primrose/StartHere*"]), { base: "../Primrose" })
-    .pipe(gulp.dest("."));
+  .pipe(gulp.dest("."));
 });
 
 X("build:primrose-debug", "cd ../Primrose && gulp debug", ["copy:primrose"]);
 
 gulp.task("cssmin", ["copy:primrose"], function () {
   return gulp.src(["stylesheets/*.css", "!stylesheets/*.min.css"])
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(cssmin())
-    .pipe(gulp.dest("stylesheets"));
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(cssmin())
+  .pipe(gulp.dest("stylesheets"));
 });
 
 gulp.task("zip:quickstart", ["copy:primrose"], function () {
   return gulp.src(["quickstart/**/*"])
-    .pipe(zip("PrimroseQuickstart.zip"))
-    .pipe(gulp.dest("."));
+  .pipe(zip("PrimroseQuickstart.zip"))
+  .pipe(gulp.dest("."));
 });
 
 function fileSize(file) {
   var size = fs.lstatSync(file).size,
-    labelIndex = 0,
-    sizeLabels = ["B", "KB", "MB", "GB", "TB"];
+  labelIndex = 0,
+  sizeLabels = ["B", "KB", "MB", "GB", "TB"];
   while (size > 1000 && labelIndex < sizeLabels.length) {
     size /= 1000;
     ++labelIndex;
@@ -61,33 +61,39 @@ function fileSize(file) {
   return size.toFixed(1) + sizeLabels[labelIndex];
 }
 
-gulp.task("pug:site", ["zip:quickstart", "build:primrose-debug"], function () {
-  return gulp.src(["*.jade"], { base: "." })
-    .pipe(rename(function (path) {
-      path.extname = "";
-      return path;
-    }))
-    .pipe(data(function (file, callback) {
-      var name = file.path.replace(/\\/g, "/"),
-        parts = name.split("/")
-          .map(function () {
-            return "../";
-          });
+function pugSite(pretty) {
+  return function(){gulp.src(["*.jade"], { base: "." })
+  .pipe(rename(function (path) {
+    path.extname = "";
+    return path;
+  }))
+  .pipe(data(function (file, callback) {
+    var name = file.path.replace(/\\/g, "/"),
+    parts = name.split("/")
+    .map(function () {
+      return "../";
+    });
 
-      parts.pop();
+    parts.pop();
 
-      callback(null, {
-        version: primroseInfo.version,
-        filePath: name,
-        cssExt: ".css",
-        jsExt: ".js",
-        fileRoot: parts.join(""),
-        fileSize: fileSize
-      });
-    }))
-    .pipe(pug())
-    .on("error", console.error.bind(console, "PUG ERROR"))
-    .pipe(gulp.dest("."));
-});
+    callback(null, {
+      version: primroseInfo.version,
+      filePath: name,
+      cssExt: ".css",
+      jsExt: ".js",
+      fileRoot: parts.join(""),
+      fileSize: fileSize
+    });
+  }))
+  .pipe(pug({
+    pretty
+  }))
+  .on("error", console.error.bind(console, "PUG ERROR"))
+  .pipe(gulp.dest("."));
+}
+}
+
+gulp.task("pug:site", ["zip:quickstart", "build:primrose-debug"], pugSite(false));
+gulp.task("just:pug:site", pugSite(true));
 
 gulp.task("default", ["pug:site", "cssmin"]);
