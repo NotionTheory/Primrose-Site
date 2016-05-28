37fdf5d92a220ab8e794877820fde9dbdad4072b
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-28 15:00:06 -0400

    cleanup and ES6-ify

--- server/socketControllers/peer.js
+++ server/socketControllers/peer.js
@@ -1,54 +1,21 @@
-var log = require("../core").log;
-
-var users = {};
+"use strict";
+const log = require("../core").log,
+ peerGroup = {};
 
 module.exports = {
   handshake: "peer",
-  bindSocket: function (socket) {
+  bindSocket: (socket) => {
     socket.on("joinRequest", function (name) {
-      if (users[name] === undefined) {
-        users[name] = [];
+      
+      // make some space for newly connected peerGroup
+      if (peerGroup[name] === undefined) {
+        peerGroup[name] = [];
       }
       
-      var sockets = users[name];
+      // get the list of sockets for the current user
+      var sockets = peerGroup[name];
       
-      function forAll(thunk) {
-        for (var i = 0; i < sockets.length; ++i) {
-          if (sockets[i]) {
-            thunk(sockets[i], i);
-          }
-        }
-      }
-      
-      function forOthers(thunk) {
-        forAll(function (skt, i) {
-          if (skt !== socket) {
-            thunk(skt, i);
-          }
-        });
-      }
-      
-      function removeSocket() {
-        for (var i = 0; i < sockets.length; ++i) {
-          if (sockets[i] === socket) {
-            sockets[i] = null;
-            break;
-          }
-        }
-      }
-      
-      socket.on("error", removeSocket);
-      socket.on("disconnect", removeSocket);
-      
-      ["offer", "answer", "ice"].forEach(function (o) {
-        socket.on(o, function (obj) {
-          var skt = sockets[obj.toIndex];
-          if (skt) {
-            skt.emit(o, obj);
-          }
-        });
-      });
-      
+      // find an empty slot
       var index;
       for (index = 0; index < sockets.length; ++index) {
         if (!sockets[index]) {
@@ -56,9 +23,54 @@ module.exports = {
         }
       }
       
+      // put the new socket in the empty slot
       sockets[index] = socket;
       
-      forOthers(function (skt, i) {
+      // wire up the WebRTC handshaking
+      const webrtc = ["offer", "answer", "ice"].map((o) => {
+        const thunk = (obj) => {
+          const skt = sockets[obj.toIndex];
+          if (skt) {
+            skt.emit(o, obj);
+          }
+        };
+        socket.on(o, thunk);
+        return {
+          name: o,
+          thunk: thunk
+        };
+      });
+
+      // we only nullify, don't remove the sockets because
+      // then we'd have to communicate the changes in index
+      // to all the peers.
+      function removeSocket() {
+        let userDisconnected = true;
+        for (let i = 0; i < sockets.length; ++i) {
+          if (sockets[i] === socket) {
+            socket.off("error", removeSocket);
+            socket.off("disconnect", removeSocket);
+            webrtc.forEach((handler) => socket.off(handler.name, handler.thunk));
+            sockets[i] = null;
+            break;
+          }
+          else if(sockets[i] !== null){
+            userDisconnected = false;
+          }
+        }
+
+        if(userDisconnected){
+          delete peerGroup[name];
+        }
+      }
+      
+      socket.on("error", removeSocket);
+      socket.on("disconnect", removeSocket);
+      
+      // notify all of the peers of the new socket
+      sockets
+      .filter((skt) => !!skt && skt !== socket)
+      .forEach((skt, i) => {
         skt.emit("user", i, index);
         socket.emit("user", index, i);
       });
