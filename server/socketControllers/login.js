5a9e1c9874fe3f8ad4bf8b4c2c75129239469ecc
Sean McBeth <Sean McBeth>
2016-06-07 10:09:00 -0400

    debug error messages

--- server/socketControllers/login.js
+++ server/socketControllers/login.js
@@ -1,19 +1,20 @@
 "use strict";
 const core = require("../core"),
-log = core.log,
-err = core.err,
-User = require("../data/User"),
-userDB = require("../data/Users"),
-activeUsers = {},
-ERROR_MESSAGE = "Invalid userName/password.";
+  log = core.log,
+  err = core.err,
+  User = require("../data/User"),
+  userDB = require("../data/Users"),
+  activeUsers = {},
+  DEBUG = process.env.ENV === "dev",
+  ERROR_MESSAGE = "Invalid userName/password.";
 
-function broadcast(evt){
+function broadcast(evt) {
   for (var key in activeUsers) {
     var toUser = activeUsers[key];
-    if (toUser.state.app === evt.app) {
+    if (toUser.app === evt.app) {
       toUser.emit
-      .bind(toUser, (toUser.state.userName === evt.userName) ? evt.skipSocketIndex : -1)
-      .apply(toUser, evt.args);
+        .bind(toUser, (toUser.userName === evt.userName) ? evt.skipSocketIndex : -1)
+        .apply(toUser, evt.args);
     }
   }
 }
@@ -22,25 +23,28 @@ module.exports = {
   handshake: "login",
   bindSocket: (socket) => {
     var key = null,
-    identity = null;
+      identity = null;
 
     function receiveHash(verb, user, hash) {
-      Promise.resolve().then(()=>{
+      Promise.resolve().then(() => {
         if (user.userName === key && (verb === "login" && user.hash === hash || verb === "signup" && user.hash === null && hash)) {
-          if(!activeUsers[key]){
+          if (!activeUsers[key]) {
             user.app = identity.app;
             activeUsers[key] = new User(user);
             activeUsers[key].addEventListener("broadcast", broadcast);
           }
-          else if (activeUsers[key].state.app !== identity.app) {
-            throw new Error(err("User [$1] tried to log into two apps: $2 and $3.", key, activeUsers[key].state.app, identity.app));
+          else if (activeUsers[key].app !== identity.app) {
+            throw new Error(err("User [$1] tried to log into two apps: $2 and $3.", key, activeUsers[key].app, identity.app));
           }
 
           activeUsers[key].addDevice(socket, activeUsers);
           user.lastLogin = new Date();
           return userDB.set(user);
         }
-        else{
+        else if (DEBUG) {
+          throw new Error(err("verb: $1, user.userName: $2, key: $3, user.hash: $4, hash: $5", verb, user.userName, key, user.hash, hash));
+        }
+        else {
           throw new Error(ERROR_MESSAGE);
         }
       }).catch((exp) => socket.emit(verb + "Failed", err(exp.message || exp)));
@@ -50,18 +54,12 @@ module.exports = {
       return (ident) => {
         identity = ident;
         key = identity
-        && identity.userName
-        && identity.userName.toLocaleUpperCase().trim();
-        if (!key) {
-          socket.emit(verb + "Failed", ERROR_MESSAGE);
-        }          
-        else {
+          && identity.userName
+          && identity.userName.toLocaleUpperCase().trim();
+        if (key) {
           log("Trying to $1 $2", verb, key);
           userDB.search(key).then((users) => {
-            if(verb === "login" && users.length === 0 || verb === "signup" && users.length > 0){
-              socket.emit(verb + "Failed", ERROR_MESSAGE);
-            }
-            else {
+            if (verb === "login" && users.length > 0 || verb === "signup" && users.length === 0) {
               var user = users[0] || {
                 userName: key,
                 salt: userDB.newSalt(),
@@ -73,7 +71,19 @@ module.exports = {
               socket.once("hash", receiveHash.bind(null, verb, user));
               socket.emit("salt", user.salt);
             }
+            else if (DEBUG) {
+              socket.emit(verb + "Failed", err("verb: $1, #users: $2", verb, users.length));
+            }
+            else {
+              socket.emit(verb + "Failed", ERROR_MESSAGE);
+            }
           });
+        }
+        else if (DEBUG) {
+          socket.emit(verb + "Failed", err("have identity: $1, userName: $2, key: $3", !!identity, identity && identity.userName, key));
+        }
+        else {
+          socket.emit(verb + "Failed", ERROR_MESSAGE);
         }
       };
     }
