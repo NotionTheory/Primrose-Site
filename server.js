eff50796b27930ada1ab37d28a88ef8a3b7fb771
Sean McBeth <Sean McBeth>
2016-06-14 12:48:48 -0400

    configure socket.io to use websockets

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ if(process.env.NODE_ENV !== "dev" || options.mode !== "localOnly"){
   const webSocketServer = require("./server/webSocketServer"),
   socketio = require("socket.io"),
   io = socketio.listen(appServer);
+  io.configure(function () {
+    io.set("transports", ["websocket", "xhr-polling"]);
+  });
   io.sockets.on("connection", webSocketServer);
 }
 
