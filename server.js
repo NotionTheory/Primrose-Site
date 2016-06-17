148989314e31bf9016cfd86b98a333f689170ba3
Sean McBeth <Sean McBeth>
2016-06-17 09:35:56 -0400

    need to run with keys on the local machine

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 "use strict";
-var err = require("./server/core").err;
+// start the HTTP server
+const options = require("./server/options").parse(process.argv),
+  fs = require("fs"),
+  http = require("http"),
+  https = require("https"),
+  path = options.path || ".",
+  webServer = require("./server/webServer")(path),
+  keys = process.env.NODE_ENV === "dev" && {
+    key: maybeGetFile("../primrosevr_com.key"),
+    cert: maybeGetFile("../primrosevr_com.crt"),
+    ca: maybeGetFile("../CACert.crt")
+  },
+  isSecure = !!(keys && keys.key && keys.cert);
 
-try{  
-  // start the HTTP server
-  var options = require("./server/options").parse(process.argv),
-    http = require("http"),
-    socketio = require("socket.io"),
-    path = options.path || ".",
-    port = options.port || process.env.PORT,
-    webServer = require("./server/webServer")(path),
-    webSocketServer = require("./server/webSocketServer"),
-    appServer = http.createServer(webServer),
-    io = socketio.listen(appServer);
-  
-  
-  console.log("Serving from directory " + path);
-  console.log("Listening on port " + port);
-  
-  appServer.listen(port);
+
+console.log("Serving from directory " + path);
+
+function maybeGetFile(file) {
+  if (fs.existsSync(file)) {
+    return fs.readFileSync(file);
+  }
+}
+
+let appServer = null;
+if (isSecure) {
+  console.log("starting secure server");
+  appServer = https.createServer(keys, webServer);
+}
+else {
+  console.log("starting insecure server", keys);
+  appServer = http.createServer(webServer)
+}
+
+const port = options.port || process.env.PORT || (isSecure ? 443 : 80);
+console.log("Listening on port " + port);
+appServer.listen(port);
+
+// start the WebSocket server
+if(process.env.NODE_ENV !== "dev" || options.mode !== "localOnly"){
+  const webSocketServer = require("./server/webSocketServer"),
+  socketio = require("socket.io"),
+  io = socketio.listen(appServer);
   io.sockets.on("connection", webSocketServer);
 }
-catch(exp){
-  err(exp.message);
+
+// start the browser
+if (process.env.NODE_ENV === "dev" && options.url) {
+  require("./server/starter")(isSecure, port, options.url);
 }
