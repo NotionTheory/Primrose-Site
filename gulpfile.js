38163b63e77db2ae7007d83ade17acf553400ff1
Sean McBeth <Sean McBeth>
2016-06-26 23:09:28 -0400

    gulpfile with watches

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ zip = require("gulp-zip"),
 
 pathX = /.*\/(.*).js/,
 hasPrimrose = fs.existsSync("../Primrose"),
-primroseInfo = hasPrimrose && require("../Primrose/package.json");
+primroseInfo = hasPrimrose && require("../Primrose/package.json"),
+stylusFiles = [not("node_modules"), "**/*.styl"],
+pugFiles = [not("node_modules"), "*.jade"];
 
 function X(name, cmd, deps){
   gulp.task(name, deps || [], function(cb){
@@ -22,6 +24,16 @@ function X(name, cmd, deps){
       console.log(stderr);
       cb(err);
     });
+  });
+}
+
+function not(dir, ext) {
+  return "!" + ((dir && dir + "/") || "") + "**/*" + (ext || "");
+}
+
+function watchify(task, files, deps) {
+  gulp.task("watch:" + task, deps || [], function () {
+    return gulp.watch(files, [task]);
   });
 }
 
@@ -35,28 +47,27 @@ if(hasPrimrose){
         f += "**/*";
       }
       return f;
-    }).concat(["!../Primrose/src/**/*", "!../Primrose/StartHere*"]),
+    }).concat([not("../Primrose/src"), "!../Primrose/StartHere*"]),
     { base: "../Primrose" })
     .pipe(gulp.dest("."));
   }
 
   gulp.task("copy:primrose", ["build:primrose"], doCopy);
   gulp.task("just:copy:primrose", doCopy);
-
-  X("build:primrose-debug", "cd ../Primrose && gulp debug", ["copy:primrose"]);
 }
 
 function doStylus(){
-  return gulp.src(["!node_modules/**/*.styl", "**/*.styl"], { base: "./" })
+  return gulp.src(stylusFiles, { base: "./" })
   .pipe(stylus())
   .pipe(gulp.dest("./"));
 }
 
 gulp.task("stylus", hasPrimrose ? ["copy:primrose"] : [], doStylus);
 gulp.task("just:stylus", doStylus);
+watchify("just:stylus", stylusFiles);
 
 function cssMin() {
-  return gulp.src(["stylesheets/*.css", "!stylesheets/*.min.css"])
+  return gulp.src(["stylesheets/**/*.css", not("stylesheets", ".min.css")])
   .pipe(rename({ suffix: ".min" }))
   .pipe(cssmin())
   .pipe(gulp.dest("stylesheets"));
@@ -86,69 +97,75 @@ function fileSize(file) {
 }
 
 function pugSite(pretty) {
+  return function(){
 
-  var files = fs.readdirSync("archive"),
-    maxMajor = 0,
-    maxMinor = 0,
-    maxRevis = 0;
+    var files = fs.readdirSync("archive"),
+      maxMajor = 0,
+      maxMinor = 0,
+      maxRevis = 0;
 
-  files.forEach(function(f){
-    var match = f.match(/Primrose-(\d+)\.(\d+)\.(\d+)\.js/);
-    if(match){
-      var major = parseFloat(match[1]),
-        minor = parseFloat(match[2]),
-        revis = parseFloat(match[3]);
-      if(major > maxMajor){
-        maxMajor = major;
-        maxMinor = 0;
-        maxRevis = 0;
-      }
-
-      if(major === maxMajor){
-        if(minor > maxMinor){
-          maxMinor = minor;
+    files.forEach(function(f){
+      var match = f.match(/Primrose-(\d+)\.(\d+)\.(\d+)\.js/);
+      if(match){
+        var major = parseFloat(match[1]),
+          minor = parseFloat(match[2]),
+          revis = parseFloat(match[3]);
+        if(major > maxMajor){
+          maxMajor = major;
+          maxMinor = 0;
           maxRevis = 0;
         }
 
-        if(minor === maxMinor){
-          maxRevis = Math.max(maxRevis, revis);
+        if(major === maxMajor){
+          if(minor > maxMinor){
+            maxMinor = minor;
+            maxRevis = 0;
+          }
+
+          if(minor === maxMinor){
+            maxRevis = Math.max(maxRevis, revis);
+          }
         }
       }
-    }
-  });
-
-  var version = maxMajor + "." + maxMinor + "." + maxRevis;
-
-  return function(){gulp.src(["*.jade"], { base: "." })
-  .pipe(rename(function (path) {
-    path.extname = "";
-    return path;
-  }))
-  .pipe(data(function (file, callback) {
-    var name = file.path.replace(/\\/g, "/"),
-    parts = name.split("/")
-    .map(function () {
-      return "../";
     });
 
-    parts.pop();
+    var version = maxMajor + "." + maxMinor + "." + maxRevis;
 
-    callback(null, {
-      version: version || "N/A",
-      filePath: name,
-      cssExt: ".css",
-      jsExt: ".js",
-      fileRoot: parts.join(""),
-      fileSize: fileSize
-    });
-  }))
-  .pipe(pug({ pretty: pretty }))
-  .on("error", console.error.bind(console, "PUG ERROR"))
-  .pipe(gulp.dest("."));
-}
+    return gulp.src(pugFiles, { base: "." })
+      .pipe(rename(function (path) {
+        path.extname = "";
+        return path;
+      }))
+      .pipe(data(function (file, callback) {
+        var name = file.path.replace(/\\/g, "/"),
+        parts = name.split("/")
+        .map(function () {
+          return "../";
+        });
+
+        parts.pop();
+
+        callback(null, {
+          version: version || "N/A",
+          filePath: name,
+          cssExt: ".css",
+          jsExt: ".js",
+          fileRoot: parts.join(""),
+          fileSize: fileSize
+        });
+      }))
+      .pipe(pug({ pretty: pretty }))
+      .on("error", console.error.bind(console, "PUG ERROR"))
+      .pipe(gulp.dest("."));
+  }
 }
 
 gulp.task("pug:site", ["zip:quickstart"], pugSite(false));
 gulp.task("just:pug:site", pugSite(true));
+watchify("just:pug:site", pugFiles);
 
-gulp.task("default", ["pug:site", "cssmin"]);
+gulp.task("debug", ["just:pug:site", "just:stylus"]);
+gulp.task("release", ["pug:site", "cssmin"]);
+gulp.task("watch", ["watch:just:pug:site", "watch:just:stylus"]);
+
+gulp.task("default", ["debug", "watch"]);
