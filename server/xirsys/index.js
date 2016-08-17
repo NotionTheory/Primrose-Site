16fb9ea01d93e1d18065a50502488076a4af4e71
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-08-17 16:00:44 -0400

    ES5-ify the things

--- server/xirsys/index.js
+++ server/xirsys/index.js
@@ -6,12 +6,13 @@ const secure = 1,
   post = require("../http/postObject"),
   del = require("../http/delObject");
 
-function req(thunk, path, params = {}){
+function req(thunk, path, params){
+  params = params || {};
   params.ident = "seanmcbeth";
   params.secret = process.env.XIRSYS_SECRET || require("../data/secrets.json").xirsysSecret;
 
   if(params.secret) {
-    let sep = "?";
+    var sep = "?";
     for(var key in params){
       path += sep + key + "=" + params[key];
       sep = "&";
@@ -31,33 +32,56 @@ function req(thunk, path, params = {}){
 
 module.exports = {
   signal: {
-    token: (application = "default", room = "default") => req(get, "signal/token", {
-      domain,
-      application,
-      room,
-      secure
+    token: (application, room) => req(get, "signal/token", {
+      domain: domain,
+      application: application || "default",
+      room: room || "default",
+      secure: secure
     }),
     list: () => req(get, "signal/list")
   },
-  ice: (application = "default", room = "default") => req(get, "ice", {
-    domain,
-    application,
-    room,
-    secure
+  ice: (application, room) => req(get, "ice", {
+    domain: domain,
+    application: application || "default",
+    room: room || "default",
+    secure: secure
   }).then((obj) => obj && obj.d),
   domain: {
     get: () => req(get, "domain"),
-    post: (domain) => req(post, "domain", { domain }),
-    del: (domain) => req(del, "domain", { domain })
+    post: (domain) => req(post, "domain", {
+      domain: domain
+    }),
+    del: (domain) => req(del, "domain", {
+      domain: domain
+    })
   },
   application: {
-    get: (domain) => req(get, "application", { domain }, get),
-    post: (domain, application) => req(post, "application", { domain, application }),
-    del: (domain, application) => req(del, "application", { domain, application })
+    get: (domain) => req(get, "application", {
+      domain: domain
+    }, get),
+    post: (domain, application) => req(post, "application", {
+      domain: domain,
+      application: application
+    }),
+    del: (domain, application) => req(del, "application", {
+      domain: domain,
+      application: application
+    })
   },
   room: {
-    get: (domain, application) => req(get, "room", { domain, application }),
-    post: (domain, application, room) => req(post, "room", { domain, application, room }),
-    del: (domain, application, room) => req(del, "room", { domain, application, room })
+    get: (domain, application) => req(get, "room", {
+      domain: domain,
+      application: application
+    }),
+    post: (domain, application, room) => req(post, "room", {
+      domain: domain,
+      application: application,
+      room: room
+    }),
+    del: (domain, application, room) => req(del, "room", {
+      domain: domain,
+      application: application,
+      room: room
+    })
   }
 };
