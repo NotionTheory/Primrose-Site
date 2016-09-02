b33625e3adbcd207465e6381876be6ee55c9850a
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-09-02 11:04:27 -0400

    v0.25.5

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
 ﻿var gulp = require("gulp"),
+  build = require("notiontheory-basic-build"),
   fs = require("fs"),
-  zip = require("gulp-zip");
+  zip = require("gulp-zip"),
+  nt = build.setup(gulp, require("./package.json"));
 
-if(fs.existsSync("../Primrose")){
-  var primroseInfo = require("../Primrose/package.json"),
-    primroseFiles = primroseInfo.files
-      .map((f) => "../Primrose/" + f.replace(/\/$/, "/**/*"))
-      .concat([
-        "!../Primorse/**/*.pug",
-        "!../Primorse/**/*.styl",
-        "!../Primrose/src/**/*",
-        "!../Primrose/StartHere*"
-      ]);
+var primroseInfo = require("./node_modules/primrose/package.json"),
+  primroseFiles = primroseInfo.files
+    .map((f) => "node_modules/primrose/" + f.replace(/\/$/, "/**/*"))
+    .concat([
+      "!node_modules/primorse/**/*.pug",
+      "!node_modules/primorse/**/*.styl",
+      "!node_modules/primrose/src/**/*",
+      "!node_modules/primrose/StartHere*"
+    ]);
 
-  gulp.task("copy:primrose", () => gulp.src(primroseFiles, { base: "../Primrose" })
-    .pipe(gulp.dest(".")));
+gulp.task("copy:primrose", () => gulp.src(primroseFiles, { base: "node_modules/Primrose" })
+  .pipe(gulp.dest(".")));
 
-  gulp.task("zip:quickstart", ["copy:primrose"], () => gulp.src(["quickstart/**/*"])
-    .pipe(zip("PrimroseQuickstart.zip"))
-    .pipe(gulp.dest(".")));
+gulp.task("zip:quickstart", ["copy:primrose"], () => gulp.src(["quickstart/**/*"])
+  .pipe(zip("PrimroseQuickstart.zip"))
+  .pipe(gulp.dest(".")));
 
-  gulp.task("default", ["zip:quickstart"]);
-}
+gulp.task("default", ["zip:quickstart"]);
+gulp.task("release", ["default"]);
