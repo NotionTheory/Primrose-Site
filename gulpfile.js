69cc5a298da1247f4e037255cb49323a705ee233
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-09-20 11:08:41 -0400

    move the archive process to the site deployment project

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 ﻿var gulp = require("gulp"),
   zip = require("gulp-zip"),
-  root = true ? "../Primrose/" : "./node_modules/primrose/";
+  rename = require("gulp-rename"),
 
-var primroseInfo = require(root + "package.json"),
+  // if we have a local copy of the library, use it.
+  fs = require("fs"),
+  root = fs.existsSync("../Primrose/") ?
+    "../Primrose/" :
+    "./node_modules/primrose/",
+
+  primroseInfo = require(root + "package.json"),
   primroseFiles = primroseInfo.files
     .map((f) => root + f.replace(/\/$/, "/**/*"))
     .concat([
@@ -15,9 +21,21 @@ var primroseInfo = require(root + "package.json"),
 gulp.task("copy:primrose", () => gulp.src(primroseFiles, { base: root })
   .pipe(gulp.dest(".")));
 
+gulp.task("archive", ["copy:primrose"], () => gulp.src(["Primrose*.js"])
+  .pipe(rename(function (file) {
+    if (file.basename.indexOf(".min") > -1) {
+      file.extname = ".min.js";
+      file.basename = file.basename.substring(0, file.basename.length - 4);
+    }
+    file.basename += "-" + primroseInfo.version;
+    return file;
+  }))
+  .pipe(gulp.dest("archive")));
+
+
 gulp.task("zip:quickstart", ["copy:primrose"], () => gulp.src(["quickstart/**/*"])
   .pipe(zip("PrimroseQuickstart.zip"))
   .pipe(gulp.dest(".")));
 
-gulp.task("default", ["zip:quickstart"]);
+gulp.task("default", ["zip:quickstart", "archive"]);
 gulp.task("release", ["default"]);
