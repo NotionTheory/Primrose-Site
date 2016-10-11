cae243ce46e21ca14033dddc35419105636eb6fe
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-11 19:37:15 -0400

    basic tokbox integration

---
+++ server/controllers/tokbox.js
@@ -0,0 +1,41 @@
+const OpenTok = require("opentok"),
+  API_KEY = process.env.OPENTOK_KEY || require("../data/secrets.json").openTokKey,
+  opentok = new OpenTok(
+    API_KEY,
+    process.env.OPENTOK_SECRET || require("../data/secrets.json").openTokSecret),
+  Message = require("../Message");
+
+function getSessionID() {
+  return new Promise((resolve, reject) => {
+    opentok.createSession((error, session) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(session);
+      }
+    });
+  });
+}
+
+const sessions = {};
+
+module.exports = {
+  URLPattern: /^\/tokbox\/?\?room=(\w+)&user=(\w+)$/,
+  GET: {
+    "*/*": (room, user, state) => {
+      console.log("tokbox", room, user);
+      if(!sessions[room]){
+        sessions[room] = getSessionID();
+      }
+      return sessions[room].then((session) => {
+        return {
+          apiKey: API_KEY,
+          sessionId: session.sessionId,
+          token: session.generateToken({
+            data: "user=" + user && user.toLocaleUpperCase()
+          })
+        };
+      }).then(Message.json);
+    }
+  }
+};
