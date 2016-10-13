bb7933bc05d44d56846a92fba07f57bbe12cb6e8
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-13 13:02:25 -0400

    a useful script

---
+++ update.sh
@@ -0,0 +1,11 @@
+cd ../Legend3D && echo "updating Legend3D" && npm update
+cd ../logger && echo "updating logger" && npm update
+cd ../notiontheory-basic-build && echo "updating notiontheory-basic-build" && npm update
+cd ../pliny && echo "updating pliny" && npm update
+cd ../Plume  && echo "updating Plume " && npm update
+cd ../Primrose && echo "updating Primrose" && npm update
+cd ../Primrose-Site && echo "updating Primrose-Site" && npm update
+cd ../replay-telemetry && echo "updating replay-telemetry" && npm update
+cd ../replay-telemetry-webvr && echo "updating replay-telemetry-webvr" && npm update
+cd ../WebVR-Bootstrapper && echo "updating WebVR-Bootstrapper" && npm update
+cd ../webvr-standard-monitor && echo "updating webvr-standard-monitor" && npm update
