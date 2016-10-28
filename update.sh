6bbd3de34b08c841cbd1e898fa9f66cb5896110b
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-28 15:11:28 -0400

    more updater updates!

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 cd ../webvr-polyfill && echo "updating webvr-polyfill" && git pull && npm install && npm update
 cd ../Legend3D && echo "updating Legend3D" && git pull && npm install && npm update
-cd ../logger && echo "updating logger" && git pull && npm install && npm update
+cd ../bare-bones-logger && echo "updating bare-bones-logger" && git pull && npm install && npm update
+cd ../notion-node && echo "updating notion-node" && git pull && npm install && npm update
 cd ../notiontheory-basic-build && echo "updating notiontheory-basic-build" && git pull && npm install && npm update
 cd ../pliny && echo "updating pliny" && git pull && npm install && npm update
+cd ../JWD && echo "updating JWD" && git pull && npm install && npm update
 cd ../Plume  && echo "updating Plume " && git pull && npm install && npm update
 cd ../Primrose && echo "updating Primrose" && git pull && npm install && npm update
 cd ../replay-telemetry && echo "updating replay-telemetry" && git pull && npm install && npm update
