bba4dfa6d56507aae6cc63558ca81a1530de8112
capnmidnight <sean.mcbeth@gmail.com>
2017-01-31 12:25:33 -0500

    switch GA tracking code to NotionTheory

--- ga.js
+++ ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-49938410-3', 'auto');
+ga('create', 'UA-54938710-6', 'auto');
 ga('send', 'pageview');
