677819c78f139ed1066c552669edb83f6ca5b3e6
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-02-06 17:56:56 -0500

    v0.31.1

---
+++ demos/empty/app.js
@@ -0,0 +1,12 @@
+const env = new Primrose.BrowserEnvironment({
+  backgroundColor: 0x000000,
+  groundTexture: "../shared_assets/images/deck.png",
+  useFog: true,
+  drawDistance: 100,
+  fullScreenButtonContainer: "#fullScreenButtonContainer",
+  nonstandardNeckLength: 0.15,
+  nonstandardNeckDepth: 0.075,
+  progress: Preloader.thunk
+});
+
+env.addEventListener("ready", Preloader.hide);
